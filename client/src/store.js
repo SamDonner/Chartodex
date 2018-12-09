6826141da3efe9c26fe069c9dd39510eb716b366
Sam Donner <sdonner33@gmail.com>
2018-12-08 23:33:24 -0800

    add redux devtools

--- client/src/store.js
+++ client/src/store.js
@@ -1,15 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
 
 const middleware = [thunk];
-const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
 
-const store = createStore(rootReducer, initialState, compose(
-  applyMiddleware(...middleware),
-  devTools
+const store = createStore(rootReducer, initialState, composeWithDevTools(
+  applyMiddleware(...middleware)
 ));
 
 export default store;
