1a5f259ae1e8a4b1c650e46950c0936e370772c7
Sam Donner <sdonner33@gmail.com>
2018-12-15 11:09:51 -0800

    add portfolio styling

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://sam:sam123@ds123434.mlab.com:23434/chartodex',
-  secretOrKey: 'secretJ',
-  CMCKey: 'b38c67b9-b860-4b54-a0ad-2686683f81c9'
+if(process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod')
+} else {
+  module.exports = require('./keys_dev')
 }
