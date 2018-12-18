3cf6733eca4bca732de77ef3c1aad5ee45fefadd
Sam Donner <sdonner33@gmail.com>
2018-12-18 11:27:18 -0800

    update log validation

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -19,7 +19,7 @@ class Landing extends  Component {
         <div className="showcase_wrap">
           <div className="container showcase"> 
             <h5 className="showcase_wrap_text"><strong>CRYPTO TRADING TOOLBOX</strong></h5>
-            <div>Easily scroll through your favorite, customizable charts, record all of your trading profits, and keep a close eye on your holdings. </div> 
+            <div>Easily scroll through your favorite, customizable charts, record your trading profits, and keep a close eye on your holdings. </div> 
             <Link to="/login">
               <button type="button" className="btn login-button shadow">
                 Get Started
